9d5cddd0f7e8d345077572cb57b4878ecafc7a40
Sally-Yitong <yitongsally@uchicago.edu>
2024-03-07 02:03:08 -0600

    Update the dodo.py (add executing notebook)

--- dodo.py
+++ dodo.py
@@ -62,28 +62,6 @@ def task_pull_rating():
         "clean": True,
     }
 
-def task_calc_spread_bias():
-    '''
-    Calculate spread and bias
-    '''
-    file_input = ['Illiq.csv.gzip', 'rating.csv']
-    file_dep = ["./src/calc_spreadBias.py"] + [DATA_DIR / "pulled" / file for file in file_input]
-    
-    file_output = ["spread_bias.csv"]
-    targets = [DATA_DIR / "pulled" / file for file in file_output]
-
-    task_dep = ["pull_trace","pull_rating"]
-
-    return {
-        "actions": [
-            "ipython ./src/calc_spreadBias.py",
-        ],
-        "targets": targets,
-        "task_dep": task_dep,
-        "file_dep": file_dep,
-        "clean": True,
-    }
-
 def task_pull_return_cs():
     '''
     pull daily return and credit spread data from Open Bond Asset Pricing website.
@@ -97,6 +75,28 @@ def task_pull_return_cs():
             "ipython ./src/load_return_cs.py",
         ],
         "targets": targets,
+        "file_dep": file_dep,
+        "clean": True,
+    }
+
+def task_calc_spread_bias():
+    '''
+    Calculate spread and bias
+    '''
+    file_input = ['Illiq.csv.gzip', 'rating.csv']
+    file_dep = ["./src/calc_spread_bias.py"] + [DATA_DIR / "pulled" / file for file in file_input]
+    
+    file_output = ["spread_bias.csv"]
+    targets = [DATA_DIR / "pulled" / file for file in file_output]
+
+    task_dep = ["pull_trace","pull_rating"]
+
+    return {
+        "actions": [
+            "ipython ./src/calc_spread_bias.py",
+        ],
+        "targets": targets,
+        "task_dep": task_dep,
         "file_dep": file_dep,
         "clean": True,
     }
@@ -126,45 +126,6 @@ def task_calc_daily_return_cs():
 
 
 
-
-
-# # def task_pull_data_via_presto():
-# #     """
-# #     Run several data pulls
-
-# #     This will run commands like this:
-# #     presto-cli --output-format=CSV_HEADER --file=/data/unixhome/src/sql_gross_performance.sql > /data/unixhome/src/sometest.csv
-
-# #     """
-# #     sql_pulls_dict = {
-# #         'sometest.sql':'sometest.csv',
-# #     }
-# #     file_dep = list(sql_pulls_dict.keys())
-# #     file_output = list(sql_pulls_dict.values())
-
-# #     targets = [PRIVATE_DATA_DIR / 'sql_pulled' / file for file in file_output]
-
-# #     def action_string(sql_file, csv_output):
-# #         s = f"""
-# #             ssh sql.someurl.com <<-'ENDSSH'
-# #             echo Starting Presto Pull Command for {sql_file}
-# #             cd {getcwd()}
-# #             presto-cli --output-format=CSV_HEADER --file={sql_file} > {csv_output}
-# #             """
-# #         return s
-# #     actions = [
-# #                 action_string(sql_file,
-# #                               (PRIVATE_DATA_DIR / 'sql_pulled' / sql_pulls_dict[sql_file])
-# #                               ) for sql_file in sql_pulls_dict
-# #             ]
-# #     return {
-# #         "actions":actions,
-# #         "targets": targets,
-# #         'task_dep':[],
-# #         "file_dep": file_dep,
-# #     }
-
-
 def task_summary_stats():
     """ """
     file_input = ['spread_bias.csv', 'daily_return_cs.csv','rating.csv']
@@ -183,117 +144,72 @@ def task_summary_stats():
     }
 
 
-# def task_example_plot():
-#     """Example plots"""
-#     file_dep = [Path("./src") / file for file in ["example_plot.py", "load_fred.py"]]
-#     file_output = ["example_plot.png"]
-#     targets = [OUTPUT_DIR / file for file in file_output]
 
-#     return {
-#         "actions": [
-#             "ipython ./src/example_plot.py",
-#         ],
-#         "targets": targets,
-#         "file_dep": file_dep,
-#         "clean": True,
-#     }
+def task_convert_notebooks_to_scripts():
+    """Preps the notebooks for presentation format.
+    Execute notebooks with summary stats and plots and remove metadata.
+    """
+    build_dir = Path(OUTPUT_DIR)
+    build_dir.mkdir(parents=True, exist_ok=True)
 
+    notebooks = [
+        "01_intraday_daily.ipynb",
+        "03_summary_stats.ipynb"
+    ]
+    file_dep = [Path("./src") / file for file in notebooks]
+    stems = [notebook.split(".")[0] for notebook in notebooks]
+    targets = [build_dir / f"_{stem}.py" for stem in stems]
 
-# def task_convert_notebooks_to_scripts():
-#     """Preps the notebooks for presentation format.
-#     Execute notebooks with summary stats and plots and remove metadata.
-#     """
-#     build_dir = Path(OUTPUT_DIR)
-#     build_dir.mkdir(parents=True, exist_ok=True)
-
-#     notebooks = [
-#         "01_example_notebook.ipynb",
-#     ]
-#     file_dep = [Path("./src") / file for file in notebooks]
-#     stems = [notebook.split(".")[0] for notebook in notebooks]
-#     targets = [build_dir / f"_{stem}.py" for stem in stems]
-
-#     actions = [
-#         # *[jupyter_execute_notebook(notebook) for notebook in notebooks_to_run],
-#         # *[jupyter_to_html(notebook) for notebook in notebooks_to_run],
-#         *[jupyter_clear_output(notebook) for notebook in stems],
-#         *[jupyter_to_python(notebook, build_dir) for notebook in stems],
-#     ]
-#     return {
-#         "actions": actions,
-#         "targets": targets,
-#         "task_dep": [],
-#         "file_dep": file_dep,
-#         "clean": True,
-#     }
+    actions = [
+        # *[jupyter_execute_notebook(notebook) for notebook in notebooks_to_run],
+        # *[jupyter_to_html(notebook) for notebook in notebooks_to_run],
+        *[jupyter_clear_output(notebook) for notebook in stems],
+        *[jupyter_to_python(notebook, build_dir) for notebook in stems],
+    ]
+    return {
+        "actions": actions,
+        "targets": targets,
+        "task_dep": [],
+        "file_dep": file_dep,
+        "clean": True,
+    }
 
 
-# def task_run_notebooks():
-#     """Preps the notebooks for presentation format.
-#     Execute notebooks with summary stats and plots and remove metadata.
-#     """
-#     notebooks = [
-#         "01_example_notebook.ipynb",
-#     ]
-#     stems = [notebook.split(".")[0] for notebook in notebooks]
+def task_run_notebooks():
+    """Preps the notebooks for presentation format.
+    Execute notebooks with summary stats and plots and remove metadata.
+    """
+    notebooks = [
+        "01_example_notebook.ipynb",
+    ]
+    stems = [notebook.split(".")[0] for notebook in notebooks]
 
-#     file_dep = [
-#         # 'load_other_data.py',
-#         *[Path(OUTPUT_DIR) / f"_{stem}.py" for stem in stems],
-#     ]
+    file_dep = [
+        # 'load_other_data.py',
+        *[Path(OUTPUT_DIR) / f"_{stem}.py" for stem in stems],
+    ]
 
-#     targets = [
-#         ## 01_example_notebook.ipynb output
-#         OUTPUT_DIR / "sine_graph.png",
-#         ## Notebooks converted to HTML
-#         *[OUTPUT_DIR / f"{stem}.html" for stem in stems],
-#     ]
+    targets = [
+        ## 03_summary_stats.ipynb output
+        OUTPUT_DIR / "summary_stats.tex",
+        ## Notebooks converted to HTML
+        *[OUTPUT_DIR / f"{stem}.html" for stem in stems],
+    ]
 
-#     actions = [
-#         *[jupyter_execute_notebook(notebook) for notebook in stems],
-#         *[jupyter_to_html(notebook) for notebook in stems],
-#         *[jupyter_clear_output(notebook) for notebook in stems],
-#         # *[jupyter_to_python(notebook, build_dir) for notebook in notebooks_to_run],
-#     ]
-#     return {
-#         "actions": actions,
-#         "targets": targets,
-#         "task_dep": [],
-#         "file_dep": file_dep,
-#         "clean": True,
-#     }
+    actions = [
+        *[jupyter_execute_notebook(notebook) for notebook in stems],
+        *[jupyter_to_html(notebook) for notebook in stems],
+        *[jupyter_clear_output(notebook) for notebook in stems],
+        # *[jupyter_to_python(notebook, build_dir) for notebook in notebooks_to_run],
+    ]
+    return {
+        "actions": actions,
+        "targets": targets,
+        "task_dep": [],
+        "file_dep": file_dep,
+        "clean": True,
+    }
 
-
-# # def task_knit_RMarkdown_files():
-# #     """Preps the RMarkdown files for presentation format.
-# #     This will knit the RMarkdown files for easier sharing of results.
-# #     """
-# #     files_to_knit = [
-# #         'shift_share.Rmd',
-# #         ]
-
-# #     files_to_knit_stems = [file.split('.')[0] for file in files_to_knit]
-
-# #     file_dep = [
-# #         'load_performance_and_loan_merged.py',
-# #         *[file + ".Rmd" for file in files_to_knit_stems],
-# #         ]
-
-# #     file_output = [file + '.html' for file in files_to_knit_stems]
-# #     targets = [OUTPUT_DIR / file for file in file_output]
-
-# #     def knit_string(file):
-# #         return f"""Rscript -e 'library(rmarkdown); rmarkdown::render("{file}.Rmd", output_format="html_document", OUTPUT_DIR="../output/")'"""
-# #     actions = [knit_string(file) for file in files_to_knit_stems]
-# #     return {
-# #         "actions": [
-# #                     "module use -a /opt/aws_opt/Modulefiles",
-# #                     "module load R/4.2.2",
-# #                     *actions],
-# #         "targets": targets,
-# #         'task_dep':[],
-# #         "file_dep": file_dep,
-# #     }
 
 
 def task_compile_latex_docs():
